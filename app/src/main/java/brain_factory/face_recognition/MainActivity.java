a345f44cfc33fffd2001450fd3888af2aae7a2ac
Cristian Cristea <cristiancristea00@gmail.com>
2021-03-31 09:33:39 +0300

    Refined the 2 buttons and added a debug text

--- app/src/main/java/brain_factory/face_recognition/MainActivity.java
+++ app/src/main/java/brain_factory/face_recognition/MainActivity.java
@@ -1,13 +1,46 @@
 package brain_factory.face_recognition;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.view.WindowManager;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity
+{
+    Button AddFaceButton;
+    Button AuthenticateButton;
+
+    TextView DebugMessage;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_activity);
+        this.getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        AuthenticateButton = findViewById(R.id.AuthenticateButton);
+        AddFaceButton = findViewById(R.id.AddFaceButton);
+
+        DebugMessage = findViewById(R.id.DebugMessage);
+        DebugMessage.setVisibility(View.GONE);
+
+        AuthenticateButton.setOnClickListener((view) ->
+        {
+            DebugMessage.setVisibility(View.VISIBLE);
+            DebugMessage.setText("The AuthenticateButton button was clicked");
+            DebugMessage.postDelayed(() -> DebugMessage.setVisibility(View.GONE), 1500);
+        });
+
+        AddFaceButton.setOnClickListener((view) ->
+        {
+            DebugMessage.setVisibility(View.VISIBLE);
+            DebugMessage.setText("The AddFaceButton button was clicked");
+            DebugMessage.postDelayed(() -> DebugMessage.setVisibility(View.GONE), 1500);
+        });
     }
 }
