60816c196c8b8352a985ca00be3ce3169b77b76e
Cristian Cristea <cristiancristea00@gmail.com>
2021-03-31 07:09:32 +0300

    Initial app commit

---
+++ app/src/main/java/brain_factory/face_recognition/MainActivity.java
@@ -0,0 +1,16 @@
+package brain_factory.face_recognition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
