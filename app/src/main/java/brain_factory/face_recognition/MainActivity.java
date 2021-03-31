fcd2bef100be8b60af08d7395a859368f30b204b
Cristian Cristea <cristiancristea00@gmail.com>
2021-03-31 07:44:50 +0300

    Used AS suggestions for ML

--- app/src/main/java/brain_factory/face_recognition/MainActivity.java
+++ app/src/main/java/brain_factory/face_recognition/MainActivity.java
@@ -1,15 +1,12 @@
 package brain_factory.face_recognition;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
